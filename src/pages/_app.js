bbec8686f8ef2c7512024bd04c6962191da503ca
1ghofrane1 <ghofranebouallegue5@gmail.com>
2025-04-09 11:56:41 +0100

    configs

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,6 @@
 import "@/styles/globals.css";
-import { AuthProvider } from "@/context/AuthContext";
+//import { AuthProvider } from "@/context/AuthContext";
 
 export default function App({ Component, pageProps }) {
-  <AuthProvider>
   return <Component {...pageProps} />;
-  </AuthProvider>
 }
